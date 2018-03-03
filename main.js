196da994934e7f061fb88cb33cc1439d6158dada
Nikita Luparev <nikita.luparev@gmail.com>
2018-03-03 11:43:13 +0500

    changed to 'for' loop

--- main.js
+++ main.js
@@ -3,18 +3,20 @@ function multiMatch(searchStr, subStrA, subStrB) {
     return -1;
   }
 
-  let i = searchStr.length - 1;
   let aIndex = -1;
   let bIndex = -1;
 
-  while (aIndex === -1 && bIndex === -1 && i > 0) {
+  for (let i = searchStr.length - 1; i > 0; i--) {
     if (searchStr[i] === subStrA) {
       aIndex = i;
     }
     if (searchStr[i] === subStrB) {
       bIndex = i;
     }
-    i = i - 1;
+
+    if (aIndex !== -1 || bIndex !== -1) {
+      break;
+    }
   }
 
   if (aIndex !== -1 && bIndex !== -1) {
